faad5d6edc4eec015b7e8b9d87b8848a4716d9cf
Marcus <noller02@hotmail.com>
2020-04-14 21:36:56 +0200

    removed direct mysql, now calls a endpoint on website instead

--- DB_Settings.py
+++
@@ -1,8 +0,0 @@
-DB_HOST = 'mysql53.unoeuro.com'
-
-DB_DATABASE = 'alpha_brawl_com_db_wowkeys'
-
-DB_USER = 'alpha_brawl_com'
-
-DB_PASS = 'Mgp150298'
-
--- background.py
+++ background.py
@@ -1,8 +1,8 @@
-from MySql import insert_key
 from lua_extracting import extract
 import json_util
 import time
 import os.path
+import requests
 # this is supposed to be the background process running. Might even be a windows service, who knows
 # every 10 min, go through the
 
@@ -29,6 +29,17 @@ def start():
             for key in lua_data:
                 insert_key(key)
         time.sleep(300)  # sleep 5 min
+
+
+def insert_key(key):
+    (name, dungeon, level) = key
+    payload = {'name': name, 'dungeon': dungeon, 'level': level}
+    url = 'https://www.alpha-brawl.com/api/insertkey'
+    #header = {'Content-type': 'application/json', "User-Agent": "XY"}
+    header = {"User-Agent": "XY"}
+    r = requests.post(url=url, data=payload, headers=header)
+
+    print(r)
 
 
 if __name__ == '__main__':
